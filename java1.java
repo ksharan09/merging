a2a5e47f0fd5c907adbea7ed0625d69db38f82b9
ksharan09 <sharanukswamy@gmail.com>
2026-07-10 09:34:42 +0530

    release updated

--- java1.java
+++ java1.java
@@ -1,6 +1,6 @@
 class Solution {
     public static void main(String[] args) {
-        System.out.println("Hello this is java 1");
+        System.out.println("Hello this is java 1 and java 1 is updated in the feature branch");
 
     }
 }
--- java2.java
+++ java2.java
@@ -1,6 +1,6 @@
 class Solution {
     public static void main(String[] args) {
-        System.out.println("Hello this is java 2");
+        System.out.println("Hello java 2 and java2 is updated in the release branch");
 
     }
 }
