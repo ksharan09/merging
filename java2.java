c269c0dd3cb5527b220f1dbfde21c05d9fbab9c1
ksharan09 <sharanukswamy@gmail.com>
2026-07-10 09:39:28 +0530

    release and merged

--- java2.java
+++ java2.java
@@ -1,6 +1,6 @@
 class Solution {
     public static void main(String[] args) {
-        System.out.println("Hello java 2 and java2 is updated in the release branch");
+        System.out.println("Hello finally changed are made in release branch and this is java 2");
 
     }
 }
